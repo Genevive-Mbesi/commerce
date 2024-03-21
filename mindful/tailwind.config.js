6e58f16e74c3680b325deb97fc0d8439e544a853
Genevive-Mbesi <genmbesi@gmail.com>
2024-03-21 14:52:24 +0300

    home

--- mindful/tailwind.config.js
+++ mindful/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ["light", "dark", "bumblebee"],
+  },
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
