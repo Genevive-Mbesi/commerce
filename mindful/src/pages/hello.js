705587d576db092565cf326418467ee30fb46baf
Genevive-Mbesi <genmbesi@gmail.com>
2024-03-28 23:38:57 +0300

    about

--- mindful/src/pages/hello.js
+++ mindful/src/pages/hello.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function Home() {
+export default function Hello() {
   return (
     <div className="hero min-h-screen bg-base-200">
    <div className="hero-content text-center">
@@ -8,7 +8,7 @@ export default function Home() {
         <h1 className="text-5xl font-bold">Mindful</h1>
         <p className="py-6">A Wellness Application designed to help manage emotions better.</p>
      <Link href='/about'>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Get </button>
      </Link>
       </div>
      
