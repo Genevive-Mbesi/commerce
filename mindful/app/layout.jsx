d2b6ed80720a0f6fb0b08a1090477091fb12c2db
mbesi <genmbesi@gmail.com>
2024-05-03 03:35:42 +0300

    setup

---
+++ mindful/app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title:'Mindful',
+    description:'A wellness application that helps understand and deal with emotions'
+}
+
+const RootLayout = () => {
+  return (
+   <html lang='eng'>
+    <body>
+        <div className='main'>
+            <div className='gradient'>
+                <main className='app'>
+                    {children}
+
+                </main>
+
+            </div>
+
+        </div>
+    </body>
+
+   </html>
+  )
+}
+
+export default RootLayout
---
+++ mindful/app/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      page
+    </div>
+  )
+}
+
+export default Home
