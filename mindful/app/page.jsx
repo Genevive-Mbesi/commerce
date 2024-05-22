4ddc89f74465866dc9f93be87b445be0db1d5212
mbesi <genmbesi@gmail.com>
2024-05-22 03:29:18 +0300

    layout

--- mindful/app/page.jsx
+++ mindful/app/page.jsx
@@ -8,9 +8,8 @@ const Home = () => {
     
       <section className='home relative w-full h-screen'> 
         <div className="flex justify-center pt-20 p-8 flex-col items-center lg:flex-row">
-        <div className="grid flex-grow h-52 card bg-purple-700 rounded-box place-items-center">content</div> 
-        <div className="divider lg:divider-horizontal"></div> 
-        <div className="grid flex-grow h-52 card bg-amber-500  rounded-box place-items-center">content</div> 
+        <div className="grid flex-grow h-52 card bg-purple-950 rounded-box place-items-center">We're MINDFUL</div> 
+        
         </div>
 
         <div class="divider"></div> 
@@ -21,7 +20,7 @@ const Home = () => {
         <div className="divider lg:divider-horizontal"></div> 
         <div className="grid flex-grow h-52 card bg-amber-300 rounded-box place-items-center">content</div> 
         <div className="divider lg:divider-horizontal"></div> 
-        <div className="grid flex-grow h-52 card bg-purple-950 rounded-box place-items-center">content</div> 
+        <div className="grid flex-grow h-52 card bg-purple-700 rounded-box place-items-center">content</div> 
         </div>
         <div class="divider"></div> 
         <div className="flex justify-center flex-col p-8 items-center lg:flex-row">
