54453bd9d9b464874d5e6f19b23ef9aacf1b72fc
mbesi <genmbesi@gmail.com>
2024-07-03 23:09:56 +0300

    shadcn card function

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,7 +6,10 @@ export default function AdminLayout({
   }>) {
     return (<>
     <Nav>
-        <NavLink href="/">Dashboard</NavLink>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
     </Nav>
     <div className="container my-6">{children}</div>
     </>
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,31 @@
+import { Card, CardDescription, CardHeader,CardTitle,CardContent } from "@/components/ui/card";
+
+
+export default function AdminDashboard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+       <DashboardCard title="Sales" subtitle="Test" body="body"/>
+    </div>
+    
+  )
+}
+
+type DashboardCardProps ={
+    title:string
+    subtitle:string
+    body:string
+}
+
+function DashboardCard({title, subtitle,body}:
+DashboardCardProps){
+    return  <Card>
+    <CardHeader>
+    <CardTitle> {title}</CardTitle>
+    <CardDescription>{subtitle}</CardDescription>
+    </CardHeader>
+    <CardContent>
+        <p>{body}</p>
+    </CardContent>
+</Card>
+}
+
