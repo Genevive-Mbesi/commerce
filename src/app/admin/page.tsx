50a19bd4f40eb631d0501293b319a006e0644c9f
mbesi <genmbesi@gmail.com>
2024-07-04 13:26:35 +0300

    Customer Card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,9 @@ import { Card, CardDescription, CardHeader,CardTitle,CardContent } from "@/compo
 import db from "@/db/db";
 import { formatCurrency,formatNumber } from "@/lib/formatters";
 
+
+
+
 async function getSalesData() {
    const data= await db.order.aggregate({
         _sum: { priceInPaidInCents: true},
@@ -13,13 +16,40 @@ async function getSalesData() {
     }
 }
 
+async function getUserData() {
+   const [userCount, orderData]= await Promise.all([
+        db.user.count(),
+      db.order.aggregate({
+        _sum: {priceInPaidInCents:true}
+      }),
+    ])
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : 
+        (orderData._sum.priceInPaidInCents || 0) / userCount / 100
+    }
+}
+
+
+
 
 export default async function AdminDashboard() {
-    const salesData = await getSalesData ()
+    const [salesData, userData] = await Promise.all([
+        getSalesData(),
+        getUserData()
+
+    ])
+   
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-       <DashboardCard title="Sales" subtitle={formatNumber(salesData.numberOfSales)} body={formatCurrency(salesData.amount)}/>
+       <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} 
+       body={formatCurrency(salesData.amount)}/>
+       <DashboardCard title="Customers" subtitle={`${formatCurrency(
+        userData.averageValuePerUser
+    )} Average Value`} 
+       body={formatNumber(userData.userCount)}/>
     </div>
+
     
   )
 }
