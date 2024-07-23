992d0e9076534a71bf801eac381b353e85e42e3d
mbesi <genmbesi@gmail.com>
2024-07-23 21:47:11 +0300

    New Product Page

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -2,10 +2,23 @@ import { PageHeader } from "../../_components/PageHeader";
 import { ProductsForm } from "../../_components/ProductsForm";
 
 export default function NewProductPage() {
+  
+  const defaultProduct = {
+    id: '',
+    name: '',
+    priceInCents: 0,
+    filePath: '',
+    imagePath: '',
+    description: '',
+    IsAvailableForPurchase: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   return (
     <>
       <PageHeader>Add Product</PageHeader>
-      <ProductsForm />
+      <ProductsForm product={defaultProduct} />
     </>
   );
 }
