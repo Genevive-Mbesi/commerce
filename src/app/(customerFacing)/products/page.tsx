eaeb560f0adfabda8579faedb2622e49ffa4a125
mbesi <genmbesi@gmail.com>
2024-07-30 21:35:52 +0300

    homepage

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,78 +1,38 @@
-import { Button } from "@/components/ui/button"
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard"
 import db from "@/db/db"
-import { formatCurrency } from "@/lib/formatters"
-import Image from "next/image"
-import Link from "next/link"
-import { notFound } from "next/navigation"
-import Stripe from "stripe"
+import { cache } from "@/lib/cache"
+import { Suspense } from "react"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
-
-export default async function SuccessPage({
-  searchParams,
-}: {
-  searchParams: { payment_intent: string }
-}) {
-  const paymentIntent = await stripe.paymentIntents.retrieve(
-    searchParams.payment_intent
-  )
-  if (paymentIntent.metadata.productId == null) return notFound()
-
-  const product = await db.product.findUnique({
-    where: { id: paymentIntent.metadata.productId },
+const getProducts = cache(() => {
+  return db.product.findMany({
+    where: {IsAvailableForPurchase: true },
+    orderBy: { name: "asc" },
   })
-  if (product == null) return notFound()
+}, ["/products", "getProducts"])
 
-  const isSuccess = paymentIntent.status === "succeeded"
-
+export default function ProductsPage() {
   return (
-    <div className="max-w-5xl w-full mx-auto space-y-8">
-      <h1 className="text-4xl font-bold">
-        {isSuccess ? "Success!" : "Error!"}
-      </h1>
-      <div className="flex gap-4 items-center">
-        <div className="aspect-video flex-shrink-0 w-1/3 relative">
-          <Image
-            src={product.imagePath}
-            fill
-            alt={product.name}
-            className="object-cover"
-          />
-        </div>
-        <div>
-          <div className="text-lg">
-            {formatCurrency(product.priceInCents / 100)}
-          </div>
-          <h1 className="text-2xl font-bold">{product.name}</h1>
-          <div className="line-clamp-3 text-muted-foreground">
-            {product.description}
-          </div>
-          <Button className="mt-4" size="lg" asChild>
-            {isSuccess ? (
-              <a
-                href={`/products/download/${await createDownloadVerification(
-                  product.id
-                )}`}
-              >
-                Download
-              </a>
-            ) : (
-              <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
-            )}
-          </Button>
-        </div>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
     </div>
   )
 }
 
-async function createDownloadVerification(productId: string) {
-  return (
-    await db.downloadVerification.create({
-      data: {
-        productId,
-        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
-      },
-    })
-  ).id
+async function ProductsSuspense() {
+  const products = await getProducts()
+
+  return products.map(product => <ProductCard key={product.id} {...product} />)
 }
