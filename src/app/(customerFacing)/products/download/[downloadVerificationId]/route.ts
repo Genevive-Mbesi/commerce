ffc3e67b65dfd6a4e400bb3d7b3d58955e1e76c7
mbesi <genmbesi@gmail.com>
2024-08-13 11:05:45 +0300

    downloadverification fix

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,6 +1,6 @@
-import db from "@/db/db";
-import { NextRequest, NextResponse } from "next/server";
-import fs from "fs/promises";
+import db from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import fs from "fs/promises"
 
 export async function GET(
   req: NextRequest,
@@ -8,42 +8,23 @@ export async function GET(
     params: { downloadVerificationId },
   }: { params: { downloadVerificationId: string } }
 ) {
-  // Log the downloadVerificationId for debugging purposes
-  console.log('DownloadVerification ID:', downloadVerificationId);
-
-  // Attempt to retrieve the DownloadVerification record
   const data = await db.downloadVerification.findUnique({
-    where: {
-      id: downloadVerificationId,
-      expiresAt: { gt: new Date() }, // Ensure the download link is still valid
-    },
-    select: {
-      product: { select: { filePath: true, name: true } },
-    },
-  });
+    where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+    select: { product: { select: { filePath: true, name: true } } },
+  })
 
-  // If no data is found, redirect to an expired page
-  if (!data || !data.product?.filePath) {
-    console.warn('Download verification not found or file path missing');
-    return NextResponse.redirect(new URL("/products/download/expired", req.url));
+  if (data == null) {
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
   }
 
-  try {
-    // Attempt to get file statistics and read the file
-    const { size } = await fs.stat(data.product.filePath);
-    const file = await fs.readFile(data.product.filePath);
-    const extension = data.product.filePath.split(".").pop();
+  const { size } = await fs.stat(data.product.filePath)
+  const file = await fs.readFile(data.product.filePath)
+  const extension = data.product.filePath.split(".").pop()
 
-    // Return the file as a response with appropriate headers
-    return new NextResponse(file, {
-      headers: {
-        "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-        "Content-Length": size.toString(),
-      },
-    });
-  } catch (error) {
-    // Log the error and redirect to an error page
-    console.error('File operation error:', error);
-    return NextResponse.redirect(new URL("/products/download/error", req.url));
-  }
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  })
 }
