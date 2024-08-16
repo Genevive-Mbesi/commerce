868a07427424f9c3ef33071c1bf257fef9d8d123
mbesi <genmbesi@gmail.com>
2024-08-16 07:19:17 +0300

    prisma update

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,8 +1,6 @@
-import db from "@/db/db";
-import { NextRequest, NextResponse } from "next/server";
-import fs from "fs/promises";
-import path from "path";
-import mime from "mime";
+import db from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import fs from "fs/promises"
 
 export async function GET(
   req: NextRequest,
@@ -10,36 +8,23 @@ export async function GET(
     params: { downloadVerificationId },
   }: { params: { downloadVerificationId: string } }
 ) {
-  try {
-    const data = await db.downloadVerification.findFirst({
-      where: {
-        id: downloadVerificationId,
-        expiresAt: { gt: new Date() },
-      },
-      select: { product: { select: { filePath: true, name: true } } },
-    });
+  const data = await db.downloadVerification.findUnique({
+    where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+    select: { product: { select: { filePath: true, name: true } } },
+  })
 
-    if (!data) {
-      return NextResponse.redirect(
-        new URL("/products/download/expired", req.url)
-      );
-    }
-
-    const resolvedPath = path.resolve(data.product.filePath);
-    const { size } = await fs.stat(resolvedPath);
-    const file = await fs.readFile(resolvedPath);
-    const extension = path.extname(resolvedPath).substring(1);
-    const mimeType = mime.getType(extension) || "application/octet-stream";
-
-    return new NextResponse(file, {
-      headers: {
-        "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-        "Content-Length": size.toString(),
-        "Content-Type": mimeType,
-      },
-    });
-  } catch (error) {
-    console.error("Error handling file download:", error);
-    return NextResponse.redirect("/500");
+  if (data == null) {
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
   }
+
+  const { size } = await fs.stat(data.product.filePath)
+  const file = await fs.readFile(data.product.filePath)
+  const extension = data.product.filePath.split(".").pop()
+
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  })
 }
