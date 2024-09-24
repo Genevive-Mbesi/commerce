a31eb62077df332768d1e9a4a7fdf38069c40cc1
mbesi <genmbesi@gmail.com>
2024-09-24 13:53:37 +0300

    postgress

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link"
 import { notFound } from "next/navigation"
 import Stripe from "stripe"
 
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function SuccessPage({
